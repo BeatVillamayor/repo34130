2fa8e16243ebd9713c12b07aec3ea5a57d7709b0
Beatriz Villamayor <irvbety@live.com.ar>
2022-12-07 23:47:43 -0300

    Repo comision 34130 con js

---
+++ comision.js/main.js
@@ -0,0 +1,29 @@
+
+//salida de datos console.log(); el arma secreta del programador
+console.log("Inicio");
+console.log(compraDeImplantes());
+
+  while (opcion != 0){
+    opcion = implantes(prompt("Ingrese Producto " + "ImplanteS"))
+
+  switch (opcion){
+         case 0;
+            console.log("Implantes Shi");
+            break;
+
+         case 0;
+            console.log("Implantes She");
+            break;
+
+        case 0;
+            console.log("Implantes Sri");
+            break;
+
+            
+         default;
+         console.log("Opcion no Valida presione boton Salir")
+          break;
+
+}
+  }
+  console.log("Gracias Por Su Visita")
