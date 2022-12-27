e456d38fe5c4f1248872709bc91be38ebfe6c011
Beatriz Villamayor <irvbety@live.com.ar>
2022-12-26 23:26:37 -0300

    voy agregando cosas a mi Carrito de js

--- comision.js/main.js
+++ comision.js/main.js
@@ -1,29 +1,72 @@
 
 //salida de datos console.log(); el arma secreta del programador
-console.log("Inicio");
+console.log("Inicio")
 console.log(compraDeImplantes());
 
   while (opcion != 0){
     opcion = implantes(prompt("Ingrese Producto " + "ImplanteS"))
 
   switch (opcion){
-         case 0:
-            console.log("Implantes Shi");
+         case "Implantes Shi":
+            cantidad = parseInt(prompt("Cuantos Implantes Shi desea Comprar?"))
+            if (resultado <= cantidad){
+            total += (cantidad = ImplantesShi.precio);
+            alert("El valor de su implante es de $" + total)
+            }
+            break;
+  
+
+        case "Implantes She":
+            cantidad = parseInt(prompt("Cuantos Implantes She desea Comprar?"))
+            if (resultado <= cantidad){
+            total += (cantidad = ImplantesShe.precio);
+               alert("El valor de su implante es de $" + total)
+            }
             break;
 
-         case 0:
-            console.log("Implantes She");
-            break;
 
-        case 0:
-            console.log("Implantes Sri");
-            break;
-
-            
-         default:
-         console.log("Opcion no Valida presione boton Salir")
-          break;
-
-}
+            case "Implantes Sri":
+              cantidad = parseInt(prompt("Cuantos Implantes Sri desea Comprar?"))
+              if (resultado <= cantidad){
+              total += (cantidad = ImplantesSri.precio);
+              alert("El valor de su implante es de $" + total)
   }
-  console.log("Gracias Por Su Visita")
+            break;
+
+
+         default:
+        (prompt("Opcion no Valida presione boton Salir"))
+          break;
+  }
+
+     console.log("Gracias Por Su Visita")
+ }
+
+    //array
+      function compraDeImplantes(array , opcion){
+      array.push(opcion)
+   }
+
+    //array suma
+          function sumar(){
+            let Totalcompra = total;
+            alert("Total de la Compra es de: $" + Totalcompra)
+
+          }
+          //objetos
+
+          function Producto(nombre, conexion, precio, stock){
+              this.nombre = nombre;
+              this.conexion = conexion;
+              this.precio = parseFloat (precio);
+              this.stock = parseInt(stock);
+
+            }
+            
+            const ImplantesShi = new Producto ("Implantes Shi", "Conexion", 8000, 30);
+            const ImplantesShe = new Producto ("Implantes She", "Conexion", 5000, 50);
+            const ImplantesSri = new Producto ("Implantes Sri", "Conexion", 7000, 40);
+          
+            
+          console.log("Gracias Por Su Visita")
+        
